703dfd7613520fe23c66da2f8485493f13a809ed
pver <peetvermeulen@gmail.com>
2018-03-06 20:49:55 +0100

    small cleanup

--- Busy/UnixDomainSocket.fs
+++ Busy/UnixDomainSocket.fs
@@ -8,15 +8,17 @@ module UnixDomainSocket =
     let private addressFamilyBytes = [|0x1uy; 0x0uy|]
     type UnixDomainSocketEndpoint(domainSocket : string) =
         inherit EndPoint()
+        
         override __.Serialize () =   let bytes = Array.concat [| addressFamilyBytes ; System.Text.Encoding.ASCII.GetBytes(domainSocket); [|0x0uy;|] |]
                                      let addr = new SocketAddress(AddressFamily.Unix, Array.length bytes)
                                      bytes |> Array.iteri (fun i b -> addr.[i] <- b)
                                      addr
+
         override __.Create (addr:SocketAddress) = 
             let bytes = Array.init (addr.Size - 3) (fun i -> addr.[i+2])
             let domainSocket = (System.Text.Encoding.ASCII.GetString bytes)
-            printfn "Creating socket for: %s" domainSocket
             new UnixDomainSocketEndpoint(domainSocket) :> EndPoint
+
         override __.AddressFamily = AddressFamily.Unix
 
     let private newline = [|13uy;10uy|]
