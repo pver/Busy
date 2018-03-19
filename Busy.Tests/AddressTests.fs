61d694ac62f583704dccb6050288b423d7052cb6
pver <peetvermeulen@gmail.com>
2018-03-19 23:10:16 +0100

    Small api cleanup

--- Busy.Tests/AddressTests.fs
+++ Busy.Tests/AddressTests.fs
@@ -7,7 +7,7 @@ open Busy.Address
 let UnixDomainSocketAddressTests =
     testList "ParsesUnixDomainSocketAddress" [
         testCase "Path address is parsed correctly" <| fun _ ->
-          let subject = parseAddress "unix:path=/tmp/dbus-test"
+          let subject = ParseAddress "unix:path=/tmp/dbus-test"
           let expected = UnixDomainSocketAddress "path=/tmp/dbus-test"
           Expect.equal subject expected "Path address is parsed correctly"
     ]
@@ -16,7 +16,7 @@ let UnixDomainSocketAddressTests =
 let TcpSocketAddressTests =
     testList "ParsesTcpSocketAddress" [
         testCase "Path address is parsed correctly" <| fun _ ->
-          let subject = parseAddress "tcp:host=127.0.0.1,port=4242"
+          let subject = ParseAddress "tcp:host=127.0.0.1,port=4242"
           let expected = TcpSocketAddress "host=127.0.0.1,port=4242"
           Expect.equal subject expected "Host+port address is parsed correctly"
     ]
--- Busy/Address.fs
+++ Busy/Address.fs
@@ -8,7 +8,7 @@ module Address =
     type NonceTcpSocketAddress = string
     type UnixExecutedSubProcessAddress = string
     type AutoLaunchAddress = string
-    type UnknownAddress = string
+    type UnsupportedAddress = string
 
     type DBusAddress =
         | UnixDomainSocketAddress of UnixDomainSocketAddress
@@ -16,16 +16,16 @@ module Address =
         | TcpSocketAddress of TcpSocketAddress
         | NonceTcpSocketAddress
         | UnixExecutedSubProcessAddress
-        | UnknownAddress of UnknownAddress
+        | InvalidAddress of string // malformed in any way
+        | UnsupportedAddress of UnsupportedAddress // correctly formed, but unknown or unsupported transporttype
         
 
-    let parseAddress (address:string) : DBusAddress =
-        let saveAddress = if System.String.IsNullOrEmpty(address) then "" else address
+    let ParseAddress (address:string) : DBusAddress =
+        let saveAddress = if System.String.IsNullOrEmpty(address) then "" else address.Trim()
 
         let stripPrefix length = saveAddress.Substring(length, saveAddress.Length-length)
 
         match saveAddress with
         | tcp when tcp.StartsWith("tcp:") -> TcpSocketAddress <| stripPrefix 4
         | unix when unix.StartsWith("unix:") -> UnixDomainSocketAddress <| stripPrefix 5
-        | _ -> UnknownAddress saveAddress
-        //UnixDomainSocketAddress (address)
+        | _ -> UnsupportedAddress saveAddress
