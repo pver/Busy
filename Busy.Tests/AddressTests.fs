35942999360fc70e0fec67829a7944098af31724
pver <peetvermeulen@gmail.com>
2018-03-21 20:46:48 +0100

    Added initial Address parsing + tests

--- Busy.Tests/AddressTests.fs
+++ Busy.Tests/AddressTests.fs
@@ -3,12 +3,48 @@ module AddressTests
 open Expecto
 open Busy.Address
 
+let createInvalidAddressTestCase name invalidValue =
+    testCase name <| fun _ ->
+          let subject = ParseAddress invalidValue
+          let expected = InvalidAddress invalidValue
+          Expect.equal subject expected name
+
+[<Tests>]
+let InvalidAddressTests =
+    testList "ParsesInvalidAddress" [
+        createInvalidAddressTestCase "Null address is marked invalid" null
+        createInvalidAddressTestCase "Empty address is marked invalid" System.String.Empty
+        createInvalidAddressTestCase "Whitespace address is marked invalid" "   "
+        createInvalidAddressTestCase "Colon only address is marked invalid" ":"
+        createInvalidAddressTestCase "Empty prefix address is marked invalid" ":name=123"
+        createInvalidAddressTestCase "Too many colon address is marked invalid" "prefix:name:name2"
+        createInvalidAddressTestCase "Invalid address is marked invalid" "abcd"
+        createInvalidAddressTestCase "Invalid formatted address is marked invalid" "unix|path=/var/test"
+
+        createInvalidAddressTestCase "Missing property key formatted address is marked invalid" "unix:=/var/test"
+        createInvalidAddressTestCase "Missing '=' after property key formatted address is marked invalid" "unix:path"
+        createInvalidAddressTestCase "Extra ',' after property formatted address is marked invalid" "unix:path=/tmp/dbus-test,"
+        createInvalidAddressTestCase "Duplicate property formatted address is marked invalid" "unix:path=abc,path=bcd"
+    ]
+
+let createUnsupportedAddressTestCase name unsupportedValue =
+    testCase name <| fun _ ->
+          let subject = ParseAddress unsupportedValue
+          let expected = UnsupportedAddress unsupportedValue
+          Expect.equal subject expected name
+
+[<Tests>]
+let UnsupportedAddressTests =
+    testList "ParsesUnsupportedAddress" [
+        createUnsupportedAddressTestCase "Unknown prefix is marked unsupported" "unknown:name=123"
+    ]    
+
 [<Tests>]
 let UnixDomainSocketAddressTests =
     testList "ParsesUnixDomainSocketAddress" [
         testCase "Path address is parsed correctly" <| fun _ ->
           let subject = ParseAddress "unix:path=/tmp/dbus-test"
-          let expected = UnixDomainSocketAddress "path=/tmp/dbus-test"
+          let expected = ValidAddress << UnixDomainSocketAddress << Map.ofSeq <| [("path","/tmp/dbus-test")]
           Expect.equal subject expected "Path address is parsed correctly"
     ]
 
@@ -17,6 +53,6 @@ let TcpSocketAddressTests =
     testList "ParsesTcpSocketAddress" [
         testCase "Path address is parsed correctly" <| fun _ ->
           let subject = ParseAddress "tcp:host=127.0.0.1,port=4242"
-          let expected = TcpSocketAddress "host=127.0.0.1,port=4242"
+          let expected = ValidAddress << TcpSocketAddress  << Map.ofSeq <| [ ("host","127.0.0.1"); ("port","4242")]
           Expect.equal subject expected "Host+port address is parsed correctly"
     ]
--- Busy/Address.fs
+++ Busy/Address.fs
@@ -2,30 +2,62 @@ namespace Busy
 
 module Address =
     
-    type UnixDomainSocketAddress = string
-    type LaunchdAddress = string
-    type TcpSocketAddress = string
-    type NonceTcpSocketAddress = string
-    type UnixExecutedSubProcessAddress = string
-    type AutoLaunchAddress = string
+    type AddressProperties = Map<string,string>
+
     type UnsupportedAddress = string
 
     type DBusAddress =
-        | UnixDomainSocketAddress of UnixDomainSocketAddress
-        | LaunchdAddress
-        | TcpSocketAddress of TcpSocketAddress
-        | NonceTcpSocketAddress
-        | UnixExecutedSubProcessAddress
+        | UnixDomainSocketAddress of AddressProperties
+        | LaunchdAddress of AddressProperties
+        | TcpSocketAddress of AddressProperties
+        | NonceTcpSocketAddress of AddressProperties
+        | UnixExecutedSubProcessAddress of AddressProperties
+        
+    type ParseAddressResult =
+        | ValidAddress of DBusAddress
+        | ParseAddressResults of ParseAddressResult[] // composite list of addresses (separated by semi colon)
         | InvalidAddress of string // malformed in any way
         | UnsupportedAddress of UnsupportedAddress // correctly formed, but unknown or unsupported transporttype
         
 
-    let ParseAddress (address:string) : DBusAddress =
+    let rec private parseSingleAddress (address:string) =
         let saveAddress = if System.String.IsNullOrEmpty(address) then "" else address.Trim()
 
-        let stripPrefix length = saveAddress.Substring(length, saveAddress.Length-length)
+        let createPrefixSpecificAddress prefix properties =
+            match prefix with
+            | "tcp" -> properties |> TcpSocketAddress |> ValidAddress
+            | "unix" -> properties |> UnixDomainSocketAddress |> ValidAddress
+            | _ -> UnsupportedAddress saveAddress
 
-        match saveAddress with
-        | tcp when tcp.StartsWith("tcp:") -> TcpSocketAddress <| stripPrefix 4
-        | unix when unix.StartsWith("unix:") -> UnixDomainSocketAddress <| stripPrefix 5
-        | _ -> UnsupportedAddress saveAddress
+        let splitPrefixFromProperties (x:string) = x.Split([|':'|])
+        let splitPropertyKeyFromValue (x:string) = x.Split([|'='|])
+
+        match splitPrefixFromProperties saveAddress with
+        | [|prefix ; settings|] when prefix <> "" -> 
+            let properties = settings.Split([|','|])
+                             |> Array.map (splitPropertyKeyFromValue >> (fun x -> 
+                                match x with
+                                | [|k; p|] when k.Trim() <> "" -> Some <| (k.Trim(),p) // TODO: unescape property parts here
+                                | _ -> None))
+
+            let propertyMap = properties 
+                              |> Array.choose id
+                              |> Map.ofArray
+
+            match propertyMap.Count with
+            | x when x = properties.Length -> createPrefixSpecificAddress prefix propertyMap
+            | _ -> InvalidAddress address // contains duplicates or properties without keys
+                        
+        | _ -> InvalidAddress address
+
+    let ParseAddress (address:string) =
+        match System.String.IsNullOrWhiteSpace(address) with 
+        | true -> InvalidAddress address 
+        | false -> 
+            let saveAddress = address.Trim()
+            let addresses = saveAddress.Split([|';'|]) |> Array.map parseSingleAddress
+
+            match addresses with
+            | [|singleAddress|] -> singleAddress
+            | [||] -> InvalidAddress address
+            | _ -> ParseAddressResults addresses
