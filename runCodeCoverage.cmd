effc78819ab78462524e89811e7ad4a9b9fea54e
pver <peetvermeulen@gmail.com>
2018-03-22 12:26:08 +0100

    Added opencoverreport to code coverage script

--- runCodeCoverage.cmd
+++ runCodeCoverage.cmd
@@ -1,4 +1,4 @@
-copy Busy.Tests\bin\Debug\netcoreapp2.0\* build
+copy .\Busy.Tests\bin\Debug\netcoreapp2.0\* build
 
 cd Busy.Tests
 dotnet minicover instrument --workdir ../codeCoverage --assemblies ../build/*.dll --sources ../**/*.fs
@@ -8,8 +8,8 @@ dotnet exec ..\build\Busy.Tests.dll
 
 dotnet minicover uninstrument --workdir ../codeCoverage 
 
-dotnet minicover htmlreport --workdir ../codeCoverage
-
-dotnet minicover report --workdir ../codeCoverage
+dotnet minicover htmlreport --workdir ../codeCoverage   --threshold 80
+dotnet minicover opencoverreport --workdir ../codeCoverage   --threshold 80
+dotnet minicover report --workdir ../codeCoverage   --threshold 80
 
 cd ..
