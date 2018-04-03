16c9eb687fc2ff86ace22091b9b64e5b0802ccfd
pver <peetvermeulen@gmail.com>
2018-04-03 19:03:07 +0200

    Filter test assemblies from results

--- runAltcover.cmd
+++ runAltcover.cmd
@@ -1,9 +1,9 @@
 del /F /Q .\codecoverage
 
-dotnet .\packages\altcover\tools\netcoreapp2.0\AltCover.dll /i=.\Busy.Tests\bin\Debug\netcoreapp2.0 /o=.\codecoverage -x=.\codecoverage\BusyCoverage.xml --opencover
+dotnet .\packages\altcover\tools\netcoreapp2.0\AltCover.dll /i=.\Busy.Tests\bin\Debug\netcoreapp2.0 /o=.\codecoverage -x=.\codecoverage\BusyCoverage.xml -s=Busy.Tests --opencover
 
 dotnet .\packages\altcover\tools\netcoreapp2.0\AltCover.dll runner -x "dotnet" -r ".\codecoverage" -- exec .\codecoverage\Busy.Tests.dll
 
 copy .\tools\ThresholdConsoleSummary.dll .\packages\ReportGenerator\tools\
 set REPORT_THRESHOLD=85
-.\packages\ReportGenerator\tools\ReportGenerator.exe "-reports:.\codecoverage\BusyCoverage.xml" "-targetdir:.\codecoverage\report" "-reporttypes:Html;ThresholdConsoleSummary" "-assemblyfilters:-Busy.Tests"
+.\packages\ReportGenerator\tools\ReportGenerator.exe "-reports:.\codecoverage\BusyCoverage.xml" "-targetdir:.\codecoverage\report" "-reporttypes:Html;ThresholdConsoleSummary"
