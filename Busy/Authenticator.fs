2fc1d68df9f170c1bfa7dfa802d8c13f7a417b01
pver <peetvermeulen@gmail.com>
2018-04-05 08:36:23 +0200

    Added Write to ITransport

--- Busy/Authenticator.fs
+++ Busy/Authenticator.fs
@@ -1,5 +1,4 @@
 namespace Busy
-open System.IO
 open Busy.Authentication
 open Transport
 
@@ -7,10 +6,9 @@ module Authenticator =
 
     let private newline = [|13uy;10uy|]
     let private formatCommand (cmd:string) = Array.append (System.Text.Encoding.ASCII.GetBytes(cmd)) newline
-    let private sendCommand (stream:Stream) cmd = 
+    let private sendCommand (transport:ITransport) cmd = 
         printfn "Writing %s" cmd
-        let bytes = formatCommand cmd 
-        stream.Write(bytes, 0, bytes.Length)
+        transport.Write <| formatCommand cmd 
 
     // Todo: 
     // Add client-server and server-client message types as discriminated union type
@@ -21,13 +19,14 @@ module Authenticator =
     //      => impl as AnonymousAuthenticator
     // Add IConnection + impl as UnixDomainSocketConnection
 
-    let private authenticateWithAuthenticator (stream:Stream) (authenticator:IDBusAauthenticator) =
+    let private authenticateWithAuthenticator (transport:ITransport) (authenticator:IDBusAauthenticator) =
       
         let rec checkStateTillCompleted state =
             match state with
             | AwaitsInput cmd -> 
-                    sendCommand stream cmd
+                    sendCommand transport cmd
                     
+                    let stream = transport.Connect() // Todo: should use byteprovider here!!
                     let bytes = Array.init 1024 (fun _ -> 0uy)
                     let bytesRec = stream.Read(bytes, 0, bytes.Length);
 
@@ -45,15 +44,14 @@ module Authenticator =
             new ExternalDBusAuthenticator("30") // 30 is hex for user id 0 (=root) TODO: replace with actual user id (platform specific!)
         ] |> List.map (fun x-> x :> IDBusAauthenticator)
 
-    // Todo: 
-    // Return IConnection here
+    // Todo: should this return a new IAuthenticatedTransport here? 
+    //       This could then later on be used to verify if the transport was authenticated
     let Authenticate (transport:ITransport) =
-        let stream = transport.Connect()
 
         let rec authenticate (authenticators:IDBusAauthenticator list) = 
             match authenticators with
             | [] -> Error "No authenticator could authenticate succesfully with the server"
-            | head::tail -> match authenticateWithAuthenticator stream head with
+            | head::tail -> match authenticateWithAuthenticator transport head with
                             | Ok o -> Ok o
                             | Error _ -> authenticate tail
 
@@ -64,7 +62,7 @@ module Authenticator =
         match authenticationResult with
         | Error _ -> authenticationResult
         | Ok _ -> 
-            sendCommand stream "BEGIN"
+            sendCommand transport "BEGIN"
             authenticationResult
 
         // Todo: call org.freedesktop.DBus.Hello, but higher up in the API, not here in connect + do this optional (BusSession type?), 
--- Busy/Transport.fs
+++ Busy/Transport.fs
@@ -11,35 +11,46 @@ module Transport =
        abstract member Connect: unit -> Stream
        abstract member IsConnected: bool with get
        abstract member Close: unit -> unit
+       abstract member Write: byte[] -> unit
 
     type UnixDomainSocketTransport (address:UnixDomainSocketAddress) =
         let mutable stream:Option<Stream> = None
 
+        let connect() =
+            match stream with
+            | Some s -> s
+            | None ->
+
+                // Todo: add checks here, could be 'abstract' or server side based path! Or better: create separate (record or union) types 
+                // to hold valid UnixDomainSocketAddress value combinations, then we just need to match here
+                let path = address.["path"] 
+                let socket = new Socket(AddressFamily.Unix, SocketType.Stream, ProtocolType.IP)
+                let endpoint = new UnixDomainSocketEndpoint(path)
+
+                socket.Connect endpoint
+                socket.Send [|0x0uy|] |> ignore // start byte required by dbus daemon as first byte
+                
+                let streamValue = new NetworkStream(socket, true) :> Stream
+                stream <- Some streamValue
+                streamValue
+        let disconnect() =
+            match stream with
+            | Some s -> s.Close()
+            | None -> ()
+
         interface ITransport with
-            member __.Connect() =
-                match stream with
-                | Some s -> s
-                | None ->
-
-                    // Todo: add checks here, could be 'abstract' or server side based path! Or better: create separate (record or union) types 
-                    // to hold valid UnixDomainSocketAddress value combinations, then we just need to match here
-                    let path = address.["path"] 
-                    let socket = new Socket(AddressFamily.Unix, SocketType.Stream, ProtocolType.IP)
-                    let endpoint = new UnixDomainSocketEndpoint(path)
-
-                    socket.Connect endpoint
-                    socket.Send [|0x0uy|] |> ignore // start byte required by dbus daemon as first byte
-                    
-                    let streamValue = new NetworkStream(socket, true) :> Stream
-                    stream <- Some streamValue
-                    streamValue
+            member __.Connect() = 
+                connect()
 
             member __.Close() =
-                match stream with
-                | Some s -> s.Close()
-                | None -> ()
-
+                disconnect()
             member __.IsConnected with get() = Option.isSome stream
+
+            member __.Write bytes =
+                match stream with
+                | Some s -> s.Write(bytes, 0, bytes.Length)
+                | None -> ()
+                
 
     let FromAddress (address:DBusAddress) : ITransport =
         match address with
