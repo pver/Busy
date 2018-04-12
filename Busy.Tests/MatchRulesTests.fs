29464a6510fddb254782bd9b37bf73d4ee5d649a
pver <peetvermeulen@gmail.com>
2018-04-12 23:38:20 +0200

    Added stringification for match rules + tests

--- Busy.Tests/MatchRulesTests.fs
+++ Busy.Tests/MatchRulesTests.fs
@@ -2,9 +2,10 @@ module MatchRulesTests
 
 open Expecto
 open Busy.MatchRules
+open Busy.MessageTypes
 
 [<Tests>]
-let tests =
+let argMatchTests =
     testList "ArgMatchIdxTests" [
         testCase "Valid values should be createable" <| fun _ ->
             let validValues = [0..63]
@@ -25,4 +26,53 @@ let tests =
 
             Expect.isEmpty createdValues "Invalid values should NOT be createable"
 
+    ]
+
+[<Tests>]
+let toMatchRuleStringTests =
+
+    testList "ToMatchRuleStringTests" [
+        testCase "SpecExample should be stringified correctly" <| fun _ ->
+            let arg2 = (ArgMatchIdx.create 2)
+            let argDict = new System.Collections.Generic.Dictionary<ArgMatchIdx, string>() 
+            
+            if Option.isSome arg2 then argDict.[(arg2.Value)] <- "bar"
+            
+            let matchRule = {
+                MatchAllRule with 
+                    Type=Some(DBusMessageType.Signal)
+                    Sender=Some("org.freedesktop.DBus")
+                    Interface=Some("org.freedesktop.DBus")
+                    Member=Some("Foo")
+                    Path=Some( PathMatchRule.Path("/bar/foo") )
+                    Destination=Some(":452345.34")
+                    Args=Some( argDict)
+                }
+            let expected = "type='signal',sender='org.freedesktop.DBus',interface='org.freedesktop.DBus',member='Foo',path='/bar/foo',destination=':452345.34',arg2='bar'"
+            let actual = toMatchRuleString matchRule
+            Expect.equal actual expected "SpecExample should be stringified correctly"
+
+        testCase "Stringified MatchAllRule should not contain anything" <| fun _ ->
+            let matchRule = MatchAllRule
+            let expected = ""
+            let actual = toMatchRuleString matchRule
+            Expect.equal actual expected "Stringified MatchAllRule should not contain anything"
+
+        testCase "Stringified PathNamespace rule should contain path_namespace" <| fun _ ->
+            let matchRule = { MatchAllRule with Path=Some( PathMatchRule.PathNamespace("/com/example/foo")) }
+            let expected = "path_namespace='/com/example/foo'"
+            let actual = toMatchRuleString matchRule
+            Expect.equal actual expected "Stringified PathNamespace rule should contain path_namespace"
+
+        testCase "Stringified Eavesdrop values rule should have correct values" <| fun _ ->
+            let actualValues = [true; false] |> List.map (fun x -> toMatchRuleString { MatchAllRule with Eavesdrop=Some(x) })
+            let expectedValues = ["eavesdrop='true'"; "eavesdrop='false'"]
+            Expect.equal actualValues expectedValues "Stringified Eavesdrop values rule should have correct values"
+
+        testCase "Stringified Type values rule should have correct values" <| fun _ ->
+            let actualValues = 
+                [DBusMessageType.Signal; DBusMessageType.MethodCall; DBusMessageType.MethodReturn; DBusMessageType.Error] 
+                |> List.map (fun x -> toMatchRuleString { MatchAllRule with Type=Some(x) })
+            let expectedValues = ["type='signal'"; "type='method_call'"; "type='method_return'"; "type='error'"]
+            Expect.equal actualValues expectedValues "Stringified Type values rule should have correct values"
     ]
--- Busy/MatchRules.fs
+++ Busy/MatchRules.fs
@@ -2,6 +2,7 @@ namespace Busy
 
 open MessageTypes
 open System.Collections.Generic
+open System.Collections.Generic
 
 module MatchRules =
 
@@ -30,5 +31,65 @@ module MatchRules =
         ArgPaths:Option<Dictionary<ArgMatchIdx, string>>;
         Arg0namespace:Option<string>;
         Eavesdrop:Option<bool>}
+
+    let MatchAllRule = {
+        Type = None; 
+        Sender = None; 
+        Interface = None; 
+        Member = None; 
+        Path = None; 
+        Destination = None; 
+        Args = None; 
+        ArgPaths = None; 
+        Arg0namespace = None; 
+        Eavesdrop = None}
+
+    let internal toMatchRuleString (rule:MatchRule) =
+
+        let formatKeyValue (key, value) = sprintf "%s='%s'" key value // todo: add escaping here!
+    
+        let typeValue = 
+            match rule.Type with
+            | Some(DBusMessageType.Signal) -> Some("signal")
+            | Some(DBusMessageType.MethodCall) -> Some("method_call")
+            | Some(DBusMessageType.MethodReturn) -> Some("method_return")
+            | Some(DBusMessageType.Error) -> Some("error")
+            | _ -> None
+
+        let pathKeyValue = 
+            match rule.Path with
+            | Some(PathMatchRule.Path p) -> Some ("path", p)
+            | Some(PathMatchRule.PathNamespace p) -> Some("path_namespace", p)
+            | None -> None
+        
+        let mapAs key = Option.map (fun x -> (key, x))
+        let mapArgIdxKeyValue idxPrefix idxSuffix (argIdxValuePair:KeyValuePair<ArgMatchIdx, string>) =
+            let key = sprintf "%s%d%s" idxPrefix (ArgMatchIdx.value argIdxValuePair.Key) idxSuffix
+            Some(key, argIdxValuePair.Value)
+
+        let argDictList idxPrefix idxSuffix dictOption = 
+            match dictOption with
+            | Some(d) -> d :> seq<_> |> Seq.map (mapArgIdxKeyValue idxPrefix idxSuffix) |> Seq.toList
+            | None -> []
+
+        let boolAsString = (fun b -> b.ToString().ToLower())
+
+        [ 
+            typeValue |> mapAs "type"
+            rule.Sender |> mapAs "sender"
+            rule.Interface |> mapAs "interface"
+            rule.Member |> mapAs "member"
+            pathKeyValue
+            rule.Destination |> mapAs "destination"
+        ] 
+        @ (rule.Args |> argDictList "arg" "")
+        @ (rule.ArgPaths |> argDictList "arg" "path")
+        @ [    
+            rule.Arg0namespace |> mapAs "arg0namespace"
+            (rule.Eavesdrop |> Option.map boolAsString |> mapAs "eavesdrop")
+        ]
+        |> List.choose id
+        |> List.map formatKeyValue
+        |> String.concat ","
 
     // Todo: add MatchRuleBuilder for C# interop purposes?
