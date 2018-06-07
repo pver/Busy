451f0aac1b1e9ea0016f0a346d6ac2fdf4f9c920
pver <peetvermeulen@gmail.com>
2018-06-07 20:54:36 +0200

    Add message iterator function and message received event

--- Busy/Bus.fs
+++ Busy/Bus.fs
@@ -16,11 +16,21 @@ type CreateBusInnerError =
 
 type CreateBusError = {CreateBusErrorMessage:string; CreateBusInnerError:option<CreateBusInnerError>}
     
+type DBusMessageReceivedEventArgs(message:DBusMessage) =
+    inherit System.EventArgs()
+    member __.Message = message
+
 type IBus =
     abstract member SendMessage: DBusMessage -> unit
     abstract member Transport: ITransport
+    abstract member IterateMessage: unit -> unit
+    
+    [<CLIEvent>]
+    abstract member DBusMessageReceived: IDelegateEvent<System.EventHandler<DBusMessageReceivedEventArgs>>
+
 
 type Bus (transport:ITransport) = 
+    let dbusMessageReceived = new DelegateEvent<System.EventHandler<DBusMessageReceivedEventArgs>>()
 
     static let getKnownBusAddress knownBus =
         match knownBus with
@@ -41,11 +51,25 @@ type Bus (transport:ITransport) =
         | InvalidAddress a -> Error { CreateBusErrorMessage="Could not retrieve a valid address"; CreateBusInnerError= Some(AddressError a) }
         | _ -> failwith "not implemented yet"
     
+
+    [<CLIEvent>]
+    member __.DBusMessageReceived = dbusMessageReceived.Publish
+    
     member __.Transport = transport
     member __.SendMessage (message:DBusMessage) =
             Marshalling.marshallMessage message
             |> transport.Write
 
+    member this.IterateMessage () =
+            let message = Unmarshalling.unmarshallMessage transport
+            match message with
+            | Ok m -> dbusMessageReceived.Trigger [|this; DBusMessageReceivedEventArgs(m)|]
+            | Error _ -> () // Todo: expose through logging or other Error event?
+            
     interface IBus with 
         member this.SendMessage (message:DBusMessage) = this.SendMessage message
         member this.Transport = this.Transport
+        member this.IterateMessage() = this.IterateMessage()
+
+        [<CLIEvent>]
+        member __.DBusMessageReceived = dbusMessageReceived.Publish
