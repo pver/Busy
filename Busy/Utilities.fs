883648a41be4620dddfe0ef590c6d0582334a882
pver <peetvermeulen@gmail.com>
2018-07-04 19:29:16 +0200

    Remove obsolete check when verifying valid object path

--- Busy/Utilities.fs
+++ Busy/Utilities.fs
@@ -24,7 +24,6 @@ module Utilities =
             
         let IsValidObjectPath path = 
             match path with
-            | "" | null -> false
             | "/" -> true
             | x -> Regex.IsMatch(x, "^(/([A-Z]|[a-z]|[0-9]|_)+)+$")
 
