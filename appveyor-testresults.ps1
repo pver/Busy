0c0592b5203d8b39a3d777f62b68c55f1631c357
pver <peetvermeulen@gmail.com>
2021-11-20 18:31:49 +0100

    Remove obsolete test results powershell script

--- appveyor-testresults.ps1
+++
@@ -1,3 +0,0 @@
-# upload test results to AppVeyor
-$wc = New-Object 'System.Net.WebClient'
-$wc.UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", (Join-Path $env:APPVEYOR_BUILD_FOLDER .\tests\Busy.Tests\bin\Debug\netcoreapp2.0\Busy.Tests.TestResults.xml))
